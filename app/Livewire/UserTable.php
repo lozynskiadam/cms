365485beb2a500bca5c9612e2d81322710cf7c4c
Adam Łożyński <adam.lozynski@alterpage.pl>
2023-11-24 00:13:06 +0100

    powergrid

--- app/Livewire/UserTable.php
+++ app/Livewire/UserTable.php
@@ -7,20 +7,28 @@ use Illuminate\Support\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
+use PowerComponents\LivewirePowerGrid\Exportable;
 use PowerComponents\LivewirePowerGrid\Facades\Filter;
 use PowerComponents\LivewirePowerGrid\Footer;
 use PowerComponents\LivewirePowerGrid\Header;
 use PowerComponents\LivewirePowerGrid\PowerGrid;
 use PowerComponents\LivewirePowerGrid\PowerGridColumns;
 use PowerComponents\LivewirePowerGrid\PowerGridComponent;
+use PowerComponents\LivewirePowerGrid\Traits\WithExport;
 
 final class UserTable extends PowerGridComponent
 {
+    use WithExport;
+
     public function setUp(): array
     {
         $this->showCheckBox();
 
         return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
             Header::make(),
             Footer::make()
                 ->showPerPage()
@@ -44,6 +52,7 @@ final class UserTable extends PowerGridComponent
             ->addColumn('id')
             ->addColumn('name')
             ->addColumn('email')
+            ->addColumn('email_verified', fn (User $model) => $model->hasVerifiedEmail())
             ->addColumn('created_at_formatted', fn (User $model) => Carbon::parse($model->created_at)->format('Y-m-d H:i'));
     }
 
@@ -61,12 +70,22 @@ final class UserTable extends PowerGridComponent
             Column::make('Email', 'email')
                 ->sortable(),
 
+            Column::make('Email zweryfikowany', 'email_verified')
+                ->toggleable(true, 'yes', 'no'),
+
             Column::make('Created at', 'created_at_formatted', 'created_at')
                 ->sortable()
                 ->searchable(),
 
             Column::action('Action')
         ];
+    }
+
+    public function onUpdatedToggleable($id, $field, $value): void
+    {
+        $user = User::findOrFail($id);
+        $user->email_verified_at = $value ? Carbon::now() : null;
+        $user->save();
     }
 
     public function filters(): array
@@ -77,6 +96,7 @@ final class UserTable extends PowerGridComponent
                 ->dataSource(User::all())
                 ->optionValue('email')
                 ->optionLabel('email'),
+            Filter::datepicker('created_at'),
         ];
     }
 
@@ -87,6 +107,9 @@ final class UserTable extends PowerGridComponent
                 ->slot('Edit')
                 ->class('btn btn-primary')
                 ->route('users.view', ['user' => $row->id]),
+            Button::add('delete')
+                ->slot('Delete')
+                ->class('btn btn-primary')
         ];
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+
+import Alpine from "alpinejs";
+import flatpickr from "flatpickr";
+
+window.Alpine = Alpine;
+window.flatpickr = flatpickr;
+
+import "./../../node_modules/flatpickr/dist/flatpickr.css";
+import './../../vendor/power-components/livewire-powergrid/dist/powergrid'
+import './../../vendor/power-components/livewire-powergrid/dist/bootstrap5.css'
