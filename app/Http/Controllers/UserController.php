f61858e9e1b73a356b750ddc2ec81484f0becf09
Adam Łożynski <adam.lozynski@alterpage.pl>
2023-11-29 16:53:54 +0100

    PowerGridTheme

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,7 @@ class UserController extends Controller
 {
     public function index(): View
     {
-        return view('pages.users.index', [
-            'models' => User::all()
-        ]);
+        return view('pages.users.index');
     }
 
     public function factory(): RedirectResponse
