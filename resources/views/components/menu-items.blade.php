3004fe46736fcce3e2781a00ce44abc4b7947be5
Adam Łożynski <lozynskiadam@gmail.com>
2024-03-08 12:06:55 +0100

    alterpress mockup

--- resources/views/components/menu-items.blade.php
+++ resources/views/components/menu-items.blade.php
@@ -1,35 +1,35 @@
 @if($root ?? false)
     <ul class="sidebar-nav">
-        @endif
-        @foreach($items as $item)
-            @if($item['url'])
-                <li class="sidebar-item @if($item['active']) active @endif ">
-                    <a
-                        href="{{ $item['url']}}"
-                        @if(empty($item['items'] ?? []))
-                            class="sidebar-link"
-                        @else
-                            class="sidebar-link @unless($item['active']) collapsed @endunless"
-                        data-bs-target="#{{ $item['id'] }}"
-                        data-bs-toggle="collapse"
-                        aria-expanded="@if($item['active']) true @else false @endif"
-                        @endif
-                    >
-                        @if($item['icon'])
-                            <i class="align-middle {{ $item['icon'] }}"></i>
-                        @endif
-                        <span class="align-middle">{{ $item['label'] }}</span>
-                    </a>
-                    @if(!empty($item['items'] ?? []))
-                        <ul id="{{ $item['id'] }}" class="sidebar-dropdown list-unstyled collapse @if($item['active']) show @endif" data-bs-parent="#sidebar">
-                            <x-menu-items :items="$item['items']"/>
-                        </ul>
-                    @endif
-                </li>
-            @else
-                <li class="sidebar-header">{{ $item['label'] }}</li>
+@endif
+@foreach($items as $item)
+    @if($item['url'])
+        <li class="sidebar-item @if($item['active']) active @endif ">
+            <a
+                href="{{ $item['url']}}"
+                @if(empty($item['items'] ?? []))
+                    class="sidebar-link"
+                @else
+                    class="sidebar-link @unless($item['active']) collapsed @endunless"
+                    data-bs-target="#{{ $item['id'] }}"
+                    data-bs-toggle="collapse"
+                    aria-expanded="@if($item['active']) true @else false @endif"
+                @endif
+            >
+                @if($item['icon'])
+                    <i class="align-middle {{ $item['icon'] }}"></i>
+                @endif
+                <span class="align-middle">{{ $item['label'] }}</span>
+            </a>
+            @if(!empty($item['items'] ?? []))
+                <ul id="{{ $item['id'] }}" class="sidebar-dropdown list-unstyled collapse @if($item['active']) show @endif" data-bs-parent="#sidebar">
+                    <x-menu-items :items="$item['items']"/>
+                </ul>
             @endif
-        @endforeach
-        @if($root ?? false)
+        </li>
+    @else
+        <li class="sidebar-header">{{ $item['label'] }}</li>
+    @endif
+@endforeach
+@if($root ?? false)
     </ul>
 @endif
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -40,6 +40,69 @@
                             <i class="fas fa-globe"></i>
                         </a>
                     </li>
+                    <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                        <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
+                            <div class="avatar avatar-online">
+                                <img src="../../assets/img/avatars/1.png" alt="" class="h-auto rounded-circle">
+                            </div>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="pages-account-settings-account.html">
+                                    <div class="d-flex">
+                                        <div class="flex-shrink-0 me-3">
+                                            <div class="avatar avatar-online">
+                                                <img src="../../assets/img/avatars/1.png" alt="" class="h-auto rounded-circle">
+                                            </div>
+                                        </div>
+                                        <div class="flex-grow-1">
+                                            <span class="fw-medium d-block">John Doe</span>
+                                            <small class="text-muted">Admin</small>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="dropdown-divider"></div>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="pages-profile-user.html">
+                                    <i class="ti ti-user-check me-2 ti-sm"></i>
+                                    <span class="align-middle">My Profile</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="pages-account-settings-account.html">
+                                    <i class="ti ti-settings me-2 ti-sm"></i>
+                                    <span class="align-middle">Settings</span>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="dropdown-divider"></div>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <i class="ti ti-help me-2 ti-sm"></i>
+                                    <span class="align-middle">FAQ</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <i class="ti ti-currency-dollar me-2 ti-sm"></i>
+                                    <span class="align-middle">Pricing</span>
+                                </a>
+                            </li>
+                            <li>
+                                <div class="dropdown-divider"></div>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#" target="_blank">
+                                    <i class="ti ti-logout me-2 ti-sm"></i>
+                                    <span class="align-middle">Log Out</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                     <li class="nav-item ms-1 dropdown">
                         <a class="nav-link dropdown-toggle position-relative" href="#" id="user-dropdown" data-bs-toggle="dropdown">
                             <i class="fas fa-user"></i>
